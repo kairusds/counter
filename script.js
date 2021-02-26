81e4c2fa4acb308d700fac05b7eb3e282d9b9dd1
harveyhans <harveyhansjavier@gmail.com>
2021-02-26 14:28:43 +0800

    dark mode detection, play/pause music dynamically, changing input of start and max outputs result, add safe integer checker

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-const songs = ["useless", "terror"];
-let songIndex = 0;
+"use strict";
 
-function $(selector){
-	return document.querySelector(selector);
+const songs = [
+	"useless_2",
+	"useless_dnb_future_bass",
+	"future_bass_thing_yuudachi_ver",
+	"terror"
+];
+let songIndex = 0;
+let songPlaying = false;
+let darkMode = false;
+
+const $ = (selector) => document.querySelector(selector);
+
+const sound = document.createElement("audio");
+sound.src = `${songs[songIndex]}.mp3`;
+sound.setAttribute("preload", "auto");
+sound.setAttribute("controls", "none");
+sound.style.display = "none";
+document.body.appendChild(sound);
+
+sound.onended = () => {
+	sound.pause();
+	songIndex++;
+	if(songIndex == songs.length) songIndex = 0;
+	sound.src = `${songs[songIndex]}.mp3`;
+	sound.play();
+};
+
+/* const playBGM = () => {
+	sound.play();
+} */
+
+const updateCounter = (event) => {
+	let min = Number($("#start").value);
+	let max = Number($("#quantity").value);
+	const numbers = [];
+	
+	if(min < Number.MIN_SAFE_INTEGER || min > Number.MAX_SAFE_INTEGER) $("#start").value = min = 0;
+	if(max > Number.MAX_SAFE_INTEGER || max < Number.MIN_SAFE_INTEGER) $("#quantity").value = max = 1;
+	
+	for(var i = min; i <= max; i++){
+		numbers.push(i);
+	}
+	
+	$("#results").value = numbers.join("\n");
 }
 
-function playBGM(){
-	const sound = document.createElement("audio");
-	sound.src = "useless.mp3";
-	sound.setAttribute("preload", "auto");
-	sound.setAttribute("controls", "none");
-	sound.style.display = "none";
-	document.body.appendChild(sound);
-	sound.play();
-	
-	sound.onended = () => {
-		sound.pause();
-		songIndex++;
-		if(songIndex == songs.length) songIndex = 0;
-		sound.src = `${songs[songIndex]}.mp3`;
-		sound.play();
-	};
+const updateTheme = () => {
+	$("body").style = `background-color: ${darkMode ? "#212121" : "#ffffff"};color: ${darkMode ? "#ffffff" : "#000000"}`;
 }
 
 $("#why").onclick = () => {
@@ -28,7 +55,7 @@ $("#why").onclick = () => {
 		$("#content").innerHTML = `
 			<br>so useless i feel nothing but emptiness growing inside me.<br>
 			creating this piece of uselessness is trash but i had fun while it lasted.<br>
-			site created by kairusds/harveyhans(me), music by <a href="https://twitter.com/BakashiLoli" target="_blank">Bakashi</a>.
+			site created by kairusds/harveyhans(me), music by <a href="https://twitter.com/ChefBakashi" target="_blank">Bakashi</a>.
 		`;
 		$("#why").innerHTML = "okay";
 	}else{
@@ -37,20 +64,41 @@ $("#why").onclick = () => {
 	}
 };
 
+$("#music").onclick = () => {
+	if(!songPlaying){
+		sound.play();
+		$("#music").innerHTML = "pause music";
+		songPlaying = true;
+	}else{
+		songPlaying = false;
+		sound.pause();
+		$("#music").innerHTML = "play music";
+	}
+}
+
 $("#dark").onchange = (event) => {
-	const checked = event.target.checked;
-	$("body").style = `background-color: ${checked ? "#212121" : "#ffffff"};color: ${checked ? "#ffffff" : "#000000"}`;
+	darkMode = event.target.checked;
+	updateTheme();
+};
+
+$("#start").oninput = (event) => {
+	updateCounter(event);
 };
 
 $("#quantity").oninput = (event) => {
-	let min = Number($("#start").value);
-	if(min < 1) $("#start").value = 0;
-	const numbers = [];
-	for(var i = min; i <= event.target.value; i++){
-		numbers.push(i);
-	}
-	$("#results").value = numbers.join("\n");
+	updateCounter(event);
 };
 
-playBGM();
-playBGM();
+if(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches){
+	darkMode = true;
+	$("#dark").checked = true;
+	updateTheme();
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
+	darkMode = event.matches;
+	$("#dark").checked = event.matches;
+	updateTheme();
+});
+
+// playBGM();
